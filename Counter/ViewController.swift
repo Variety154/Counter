fe378ee1cba4e2fc0e4125c9e4a84eafab9f533d
Variety154 <varvarakit15042103@gmail.com>
2023-12-27 13:16:51 +0700

    Создание проекта

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var lable: UILabel!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count += 1
+        lable.text = "Значение счётчика: \(count)"
+    }
+    
+    
 }
 
